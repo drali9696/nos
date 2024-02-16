99b935ceea9ae35c12bd4dca06a5a470eb3e1c51
drali9696 <154887727+drali9696@users.noreply.github.com>
2024-02-16 23:37:20 +0330

    Update app.config.ts

--- src/common/app.config.ts
+++ src/common/app.config.ts
@@ -8,23 +8,23 @@
  */
 export const Brand = {
   Title: {
-    Base: 'big-AGI',
-    Common: (process.env.NODE_ENV === 'development' ? '[DEV] ' : '') + 'big-AGI',
+    Base: 'noskhe',
+    Common: (process.env.NODE_ENV === 'development' ? ' ' : '') + 'noskhe',
   },
   Meta: {
-    Description: 'Launch big-AGI to unlock the full potential of AI, with precise control over your data and models. Voice interface, AI personas, advanced features, and fun UX.',
-    SiteName: 'big-AGI | Precision AI for You',
+    Description: 'Best Medical AI Assistant',
+    SiteName: 'noskhe | Precision AI for You',
     ThemeColor: '#32383E',
-    TwitterSite: '@enricoros',
+    TwitterSite: '',
   },
   URIs: {
-    Home: 'https://big-agi.com',
-    // App: 'https://get.big-agi.com',
-    CardImage: 'https://big-agi.com/icons/card-dark-1200.png',
+    Home: 'https://noskhe.chat',
+    // App: 'https://noskhe.chat',
+    CardImage: '',
     OpenRepo: 'https://github.com/enricoros/big-agi',
     OpenProject: 'https://github.com/users/enricoros/projects/4',
-    SupportInvite: 'https://discord.gg/MkH4qj2Jp9',
-    // Twitter: 'https://www.twitter.com/enricoros',
-    PrivacyPolicy: 'https://big-agi.com/privacy',
+    SupportInvite: 'https://noskhe.chat',
+    // Twitter: 'https://noskhe.chat',
+    PrivacyPolicy: 'https://noskhe.chat',
   },
 };
