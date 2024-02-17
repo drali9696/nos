49cf0c8f2dfad3623ca5f5b6c502c37981e4c354
drali9696 <154887727+drali9696@users.noreply.github.com>
2024-02-18 00:38:47 +0330

    Update modelUtils.ts

--- src/common/util/modelUtils.ts
+++ src/common/util/modelUtils.ts
@@ -6,14 +6,14 @@ function getModelFromFile(filePath: string): string {
 
 export function prettyBaseModel(model: string | undefined): string {
   if (!model) return '';
-  if (model.includes('gpt-4-vision-preview')) return 'GPT-4 Vision';
-  if (model.includes('gpt-4-1106-preview')) return 'GPT-4 Turbo';
-  if (model.includes('gpt-4-32k')) return 'gpt-4-32k';
-  if (model.includes('gpt-4')) return 'gpt-4';
-  if (model.includes('gpt-3.5-turbo-instruct')) return '3.5 Turbo Instruct';
-  if (model.includes('gpt-3.5-turbo-1106')) return '3.5 Turbo 16k';
-  if (model.includes('gpt-3.5-turbo-16k')) return '3.5 Turbo 16k';
-  if (model.includes('gpt-3.5-turbo')) return '3.5 Turbo';
+  if (model.includes('gpt-4-vision-preview')) return 'version 2';
+  if (model.includes('gpt-4-1106-preview')) return 'version 2';
+  if (model.includes('gpt-4-32k')) return 'version 2';
+  if (model.includes('gpt-4')) return 'version 2';
+  if (model.includes('gpt-3.5-turbo-instruct')) return 'version 1';
+  if (model.includes('gpt-3.5-turbo-1106')) return 'version 1';
+  if (model.includes('gpt-3.5-turbo-16k')) return 'version 1';
+  if (model.includes('gpt-3.5-turbo')) return 'version 1';
   if (model.endsWith('.bin')) return model.slice(0, -4);
   // [LM Studio]
   if (model.startsWith('C:\\') || model.startsWith('D:\\'))
