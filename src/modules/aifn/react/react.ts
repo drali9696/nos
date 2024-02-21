821fe6596b41bc1d4f2206aaae445b390a64a077
alieali <121768238+alieali@users.noreply.github.com>
2024-02-21 15:16:55 +0330

    Update react.ts

--- src/modules/aifn/react/react.ts
+++ src/modules/aifn/react/react.ts
@@ -1,209 +1,268 @@
-/*
- * porting of implementation from here: https://til.simonwillison.net/llms/python-react-pattern
- */
+import * as React from 'react';
+import { useQuery } from '@tanstack/react-query';
 
-import type { DLLMId } from '~/modules/llms/store-llms';
-import { bareBonesPromptMixer } from '~/modules/persona/pmix/pmix';
-import { callApiSearchGoogle } from '~/modules/google/search.client';
-import { callBrowseFetchPage } from '~/modules/browse/browse.client';
-import { llmChatGenerateOrThrow, VChatMessageIn } from '~/modules/llms/llm.client';
+import type { SxProps } from '@mui/joy/styles/types';
+import { Box, ButtonGroup, IconButton, Sheet, Tooltip, Typography } from '@mui/joy';
+import ContentCopyIcon from '@mui/icons-material/ContentCopy';
+import FitScreenIcon from '@mui/icons-material/FitScreen';
+import HtmlIcon from '@mui/icons-material/Html';
+import SchemaIcon from '@mui/icons-material/Schema';
+import ShapeLineOutlinedIcon from '@mui/icons-material/ShapeLineOutlined';
 
+import { copyToClipboard } from '~/common/util/clipboardUtils';
+import { frontendSideFetch } from '~/common/util/clientFetchers';
 
-// prompt to implement the ReAct paradigm: https://arxiv.org/abs/2210.03629
-const reActPrompt = (enableBrowse: boolean): string =>
-  `You are a Question Answering AI with reasoning ability.
-You will receive a Question from the User.
-In order to answer any Question, you run in a loop of Thought, Action, PAUSE, Observation.
-If from the Thought or Observation you can derive the answer to the Question, you MUST also output an "Answer: ", followed by the answer and the answer ONLY, without explanation of the steps used to arrive at the answer.
-You will use "Thought: " to describe your thoughts about the question being asked.
-You will use "Action: " to run one of the actions available to you - then return PAUSE. NEVER continue generating "Observation: " or "Answer: " in the same response that contains PAUSE.
-"Observation" will be presented to you as the result of previous "Action".
-If the "Observation" you received is not related to the question asked, or you cannot derive the answer from the observation, change the Action to be performed and try again.
-
-ALWAYS assume today as {{Today}} when dealing with questions regarding dates.
-Never mention your knowledge cutoff date
-
-Your available "Actions" are:
-
-google:
-e.g. google: Django
-Returns google custom search results
-ALWAYS look up on google when the question is related to live events or factual information, such as sports, news, or weather.
-
-` + (enableBrowse ? `loadUrl:
-e.g. loadUrl: https://arxiv.org/abs/1706.03762
-Opens the given URL and displays it
-
-` : '') + `calculate:
-e.g. calculate: 4 * 7 / 3
-Runs a calculation and returns the number - uses Python so be sure to use floating point syntax if necessary
-
-wikipedia:
-e.g. wikipedia: Django
-Returns a summary from searching Wikipedia
-
-ONLY look things up on Wikipedia when explicitly asked to do so.
-
-Example session:
-
-Question: What is the capital of France?
-Thought: I should look up France on Wikipedia
-Action: wikipedia: France
-
-You will be called again with the following, along with all previous messages between the User and You:
-
-Observation: France is a country. The capital is Paris.
-
-You then output:
-Answer: The capital of France is Paris
-`;
+import type { CodeBlock } from '../blocks';
+import { ButtonCodePen, isCodePenSupported } from './ButtonCodePen';
+import { ButtonJsFiddle, isJSFiddleSupported } from './ButtonJSFiddle';
+import { ButtonStackBlitz, isStackBlitzSupported } from './ButtonStackBlitz';
+import { heuristicIsHtml, IFrameComponent } from '../RenderHtml';
+import { patchSvgString, RenderCodeMermaid } from './RenderCodeMermaid';
 
 
-const actionRe = /^Action: (\w+): (.*)$/;
-
-
-/**
- * State - Abstraction used for serialization, save/restore, inspection, debugging, rendering, etc.
- *
- * Keep this as minimal and flat as possible
- *   - initialize(): will create the state with initial values
- *   - loop() is a function that will update the state (in place)
- */
-interface State {
-  messages: VChatMessageIn[];
-  nextPrompt: string;
-  lastObservation: string;
-  result: string | undefined;
-}
-
-export class Agent {
-
-  // NOTE: this is here for demo, but the whole loop could be moved to the caller's event loop
-  async reAct(question: string, llmId: DLLMId, maxTurns = 5, enableBrowse = false,
-              appendLog: (...data: any[]) => void = console.log,
-              showState: (state: object) => void): Promise<string> {
-    let i = 0;
-    // TODO: to initialize with previous chat messages to provide context.
-    const S: State = this.initialize(`Question: ${question}`, llmId, enableBrowse, appendLog);
-    showState(S);
-    while (i < maxTurns && S.result === undefined) {
-      i++;
-      appendLog(`\n## Turn ${i}`);
-      await this.step(S, llmId, appendLog);
-      showState(S);
-    }
-    // return only the 'Answer: ' part of the result
-    if (S.result) {
-      const idx = S.result.indexOf('Answer: ');
-      if (idx !== -1)
-        return S.result.slice(idx + 8);
-    }
-    return S.result || 'No result';
-  }
-
-  initialize(question: string, assistantLLMId: DLLMId, enableBrowse: boolean, log: (...data: any[]) => void = console.log): State {
-    const state: State = {
-      messages: [{ role: 'system', content: bareBonesPromptMixer(reActPrompt(enableBrowse), assistantLLMId) }],
-      nextPrompt: question,
-      lastObservation: '',
-      result: undefined,
-    };
-    log('## Prepare Buffer');
-    for (let i = 0; i < state.messages.length; i++)
-      log('→ ' + state.messages[i].role + ' [' + (i + 1) + ']: "' + state.messages[i].content.slice(0, 86).replaceAll('\n', ' ') + ' ..."');
-    return state;
-  }
-
-  truncateStringAfterPause(input: string): string {
-    const pauseKeyword = 'PAUSE';
-    const pauseIndex = input.indexOf(pauseKeyword);
-
-    if (pauseIndex === -1) {
-      return input;
-    }
-
-    const endIndex = pauseIndex + pauseKeyword.length;
-    return input.slice(0, endIndex);
-  }
-
-  async chat(S: State, prompt: string, llmId: DLLMId): Promise<string> {
-    S.messages.push({ role: 'user', content: prompt });
-    let content: string;
-    try {
-      content = (await llmChatGenerateOrThrow(llmId, S.messages, null, null, 500)).content;
-    } catch (error: any) {
-      content = `Error in llmChatGenerateOrThrow: ${error}`;
-    }
-    // process response, strip out potential hallucinated response after PAUSE is detected
-    content = this.truncateStringAfterPause(content);
-    S.messages.push({ role: 'assistant', content });
-    return content;
-  }
-
-  async step(S: State, llmId: DLLMId, log: (...data: any[]) => void = console.log) {
-    log('→ ' + (S.lastObservation ? 'action' : 'user') + ' [' + (S.messages.length + 1) + ']: "' + S.nextPrompt + '"');
-    const result = await this.chat(S, S.nextPrompt, llmId);
-    log('← reAct [' + (S.messages.length) + ']: "' + result + '"');
-    const actions = result
-      .split('\n')
-      .map((a: string) => actionRe.exec(a))
-      .filter((a: RegExpExecArray | null) => a !== null) as RegExpExecArray[];
-    if (actions.length > 0) {
-      const action = actions[0][1];
-      const actionInput = actions[0][2];
-      if (!(action in knownActions)) {
-        throw new Error(`Unknown action: ${action}: ${actionInput}`);
-      }
-      log(`⚡ __${action}__("${actionInput}") → Observation`);
-      S.lastObservation = await knownActions[action](actionInput);
-      S.nextPrompt = `Observation: ${S.lastObservation}`;
-      // will be displayed in the next step
-      // log('=>' + S.nextPrompt);
-    } else {
-      log('↙ done');
-      // already displayed (← react)
-      // log(`Result: ${result}`);
-      S.result = result;
-    }
-  }
-}
-
-
-type ActionFunction = (input: string) => Promise<string>;
-
-async function wikipedia(q: string): Promise<string> {
-  const response = await fetch(
-    `https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=${encodeURIComponent(q)}&format=json&origin=*`,
-  );
-  const data = await response.json();
-  return data.query.search[0].snippet;
-}
-
-async function search(query: string): Promise<string> {
+async function fetchPlantUmlSvg(plantUmlCode: string): Promise<string | null> {
+  // fetch the PlantUML SVG
+  let text: string = '';
   try {
-    const data = await callApiSearchGoogle(query);
-    return JSON.stringify(data);
-  } catch (error: any) {
-    console.error('Error fetching search results:', error);
-    return 'An error occurred while searching the internet. Missing Google API Key? Google error: ' + (error?.message || error?.toString() || 'Unknown error');
+    // Dynamically import the PlantUML encoder - it's a large library that slows down app loading
+    const { encode: plantUmlEncode } = await import('plantuml-encoder');
+
+    // retrieve and manually adapt the SVG, to remove the background
+    const encodedPlantUML: string = plantUmlEncode(plantUmlCode);
+    const response = await frontendSideFetch(`https://www.plantuml.com/plantuml/svg/${encodedPlantUML}`);
+    text = await response.text();
+  } catch (e) {
+    return null;
   }
+  // validate/extract the SVG
+  const start = text.indexOf('<svg ');
+  const end = text.indexOf('</svg>');
+  if (start < 0 || end <= start)
+    throw new Error('Could not render PlantUML');
+  const svg = text
+    .slice(start, end + 6) // <svg ... </svg>
+    .replace('background:#FFFFFF;', ''); // transparent background
+
+  // check for syntax errors
+  if (svg.includes('>Syntax Error?</text>'))
+    throw new Error('syntax issue (it happens!). Please regenerate or change generator model.');
+
+  return svg;
 }
 
-async function browse(url: string): Promise<string> {
-  try {
-    const page = await callBrowseFetchPage(url);
-    return JSON.stringify(page.content ? { text: page.content } : { error: 'Issue reading the page' });
-  } catch (error) {
-    console.error('Error browsing:', (error as Error).message);
-    return 'An error occurred while browsing to the URL. Missing WSS Key?';
-  }
-}
 
-const calculate = async (what: string): Promise<string> => String(eval(what));
-
-const knownActions: { [key: string]: ActionFunction } = {
-  wikipedia: wikipedia,
-  google: search,
-  loadUrl: browse,
-  calculate: calculate,
+export const overlayButtonsSx: SxProps = {
+  position: 'absolute', top: 0, right: 0, zIndex: 10,
+  display: 'flex', flexDirection: 'row', gap: 1,
+  opacity: 0, transition: 'opacity 0.2s',
+  // '& > button': {
+  // backgroundColor: 'background.level2',
+  // backdropFilter: 'blur(12px)',
+  // },
 };
+
+
+interface RenderCodeBaseProps {
+  codeBlock: CodeBlock,
+  isMobile?: boolean,
+  noCopyButton?: boolean,
+  optimizeLightweight?: boolean,
+  sx?: SxProps,
+}
+
+interface RenderCodeImplProps extends RenderCodeBaseProps {
+  highlightCode: (inferredCodeLanguage: string | null, blockCode: string) => string,
+  inferCodeLanguage: (blockTitle: string, code: string) => string | null,
+}
+
+function RenderCodeImpl(props: RenderCodeImplProps) {
+
+  // state
+  const [fitScreen, setFitScreen] = React.useState(!!props.isMobile);
+  const [showHTML, setShowHTML] = React.useState(false);
+  const [showMermaid, setShowMermaid] = React.useState(true);
+  const [showPlantUML, setShowPlantUML] = React.useState(true);
+  const [showSVG, setShowSVG] = React.useState(true);
+
+  // derived props
+  const {
+    codeBlock: { blockTitle, blockCode, complete: blockComplete },
+    highlightCode, inferCodeLanguage,
+    optimizeLightweight,
+  } = props;
+
+  // heuristic for language, and syntax highlight
+  const { highlightedCode, inferredCodeLanguage } = React.useMemo(() => {
+    const inferredCodeLanguage = inferCodeLanguage(blockTitle, blockCode);
+    const highlightedCode = highlightCode(inferredCodeLanguage, blockCode);
+    return { highlightedCode, inferredCodeLanguage };
+  }, [inferCodeLanguage, blockTitle, blockCode, highlightCode]);
+
+
+  // heuristics for specialized rendering
+
+  const isHTML = heuristicIsHtml(blockCode);
+  const renderHTML = isHTML && showHTML;
+
+  const isMermaid = blockTitle === 'mermaid' && blockComplete;
+  const renderMermaid = isMermaid && showMermaid;
+
+  const isPlantUML =
+    (blockCode.startsWith('@startuml') && blockCode.endsWith('@enduml'))
+    || (blockCode.startsWith('@startmindmap') && blockCode.endsWith('@endmindmap'))
+    || (blockCode.startsWith('@startsalt') && blockCode.endsWith('@endsalt'))
+    || (blockCode.startsWith('@startwbs') && blockCode.endsWith('@endwbs'))
+    || (blockCode.startsWith('@startgantt') && blockCode.endsWith('@endgantt'));
+
+  let renderPlantUML = isPlantUML && showPlantUML;
+  const { data: plantUmlHtmlData, error: plantUmlError } = useQuery({
+    enabled: renderPlantUML,
+    queryKey: ['plantuml', blockCode],
+    queryFn: () => fetchPlantUmlSvg(blockCode),
+    staleTime: 24 * 60 * 60 * 1000, // 1 day
+  });
+  renderPlantUML = renderPlantUML && (!!plantUmlHtmlData || !!plantUmlError);
+
+  const isSVG = blockCode.startsWith('<svg') && blockCode.endsWith('</svg>');
+  const renderSVG = isSVG && showSVG;
+  const canScaleSVG = renderSVG && blockCode.includes('viewBox="');
+
+
+  const canCodePen = blockComplete && isCodePenSupported(inferredCodeLanguage, isSVG);
+  const canJSFiddle = blockComplete && isJSFiddleSupported(inferredCodeLanguage, blockCode);
+  const canStackBlitz = blockComplete && isStackBlitzSupported(inferredCodeLanguage);
+
+
+  const handleCopyToClipboard = (e: React.MouseEvent) => {
+    e.stopPropagation();
+    copyToClipboard(blockCode, 'Code');
+  };
+
+  return (
+    <Box sx={{
+      position: 'relative', /* for overlay buttons to stick properly */
+    }}>
+
+      {/* Code render */}
+      <Box
+        component='code'
+        className={`language-${inferredCodeLanguage || 'unknown'}`}
+        sx={{
+          fontWeight: 500, whiteSpace: 'pre', // was 'break-spaces' before we implemented per-block scrolling
+          mx: 0, p: 1.5, // this block gets a thicker border
+          display: 'block',
+          overflowX: 'auto',
+          minWidth: 160,
+          '&:hover > .overlay-buttons': { opacity: 1 },
+          ...(props.sx || {}),
+        }}>
+
+        {/* Markdown Title (File/Type) */}
+        {blockTitle != inferredCodeLanguage && blockTitle.includes('.') && (
+          <Sheet sx={{ boxShadow: 'sm', borderRadius: 'sm', mb: 1 }}>
+            <Typography level='title-sm' sx={{ px: 1, py: 0.5 }}>
+              {blockTitle}
+              {/*{inferredCodeLanguage}*/}
+            </Typography>
+          </Sheet>
+        )}
+
+        {/* Renders HTML, or inline SVG, inline plantUML rendered, or highlighted code */}
+        {renderHTML
+          ? <IFrameComponent htmlString={blockCode} />
+          : renderMermaid
+            ? <RenderCodeMermaid mermaidCode={blockCode} fitScreen={fitScreen} />
+            : <Box component='div'
+                   dangerouslySetInnerHTML={{
+                     __html:
+                       renderSVG
+                         ? (patchSvgString(fitScreen, blockCode) || 'No SVG code')
+                         : renderPlantUML
+                           ? (patchSvgString(fitScreen, plantUmlHtmlData) || (plantUmlError as string) || 'No PlantUML rendering.')
+                           : highlightedCode,
+                   }}
+                   sx={{
+                     ...(renderSVG ? { lineHeight: 0 } : {}),
+                     ...(renderPlantUML ? { textAlign: 'center' } : {}),
+                   }}
+            />}
+
+        {/* Buttons */}
+        <Box className='overlay-buttons' sx={{ ...overlayButtonsSx, p: 0.5 }}>
+          {isHTML && (
+            <Tooltip title={optimizeLightweight ? null : renderHTML ? 'Hide' : 'Show Web Page'}>
+              <IconButton variant={renderHTML ? 'solid' : 'soft'} color='danger' onClick={() => setShowHTML(!showHTML)}>
+                <HtmlIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          {isMermaid && (
+            <Tooltip title={optimizeLightweight ? null : renderMermaid ? 'Show Code' : 'Render Mermaid'}>
+              <IconButton variant={renderMermaid ? 'solid' : 'soft'} onClick={() => setShowMermaid(!showMermaid)}>
+                <SchemaIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          {isPlantUML && (
+            <Tooltip title={optimizeLightweight ? null : renderPlantUML ? 'Show Code' : 'Render PlantUML'}>
+              <IconButton variant={renderPlantUML ? 'solid' : 'soft'} onClick={() => setShowPlantUML(!showPlantUML)}>
+                <SchemaIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          {isSVG && (
+            <Tooltip title={optimizeLightweight ? null : renderSVG ? 'Show Code' : 'Render SVG'}>
+              <IconButton variant={renderSVG ? 'solid' : 'soft'} onClick={() => setShowSVG(!showSVG)}>
+                <ShapeLineOutlinedIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          {((isMermaid && showMermaid) || (isPlantUML && showPlantUML) || (isSVG && showSVG && canScaleSVG)) && (
+            <Tooltip title={optimizeLightweight ? null : fitScreen ? 'Original Size' : 'Fit Screen'}>
+              <IconButton variant={fitScreen ? 'solid' : 'soft'} onClick={() => setFitScreen(on => !on)}>
+                <FitScreenIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+          {(canJSFiddle || canCodePen || canStackBlitz) && (
+            <ButtonGroup aria-label='Open code in external editors' sx={{ cornerRadius: 'md' }}>
+              {canJSFiddle && <ButtonJsFiddle code={blockCode} language={inferredCodeLanguage!} />}
+              {canCodePen && <ButtonCodePen code={blockCode} language={inferredCodeLanguage!} />}
+              {canStackBlitz && <ButtonStackBlitz code={blockCode} title={blockTitle} language={inferredCodeLanguage!} />}
+            </ButtonGroup>
+          )}
+          {props.noCopyButton !== true && (
+            <Tooltip title={optimizeLightweight ? null : 'Copy Code'}>
+              <IconButton variant='soft' onClick={handleCopyToClipboard}>
+                <ContentCopyIcon />
+              </IconButton>
+            </Tooltip>
+          )}
+        </Box>
+
+      </Box>
+    </Box>
+  );
+}
+
+// Dynamically import the heavy prism functions
+const RenderCodeDynamic = React.lazy(async () => {
+
+  // Dynamically import the code highlight functions
+  const { highlightCode, inferCodeLanguage } = await import('./codePrism');
+
+  return {
+    default: (props: RenderCodeBaseProps) =>
+      <RenderCodeImpl highlightCode={highlightCode} inferCodeLanguage={inferCodeLanguage} {...props} />,
+  };
+});
+
+export function RenderCode(props: RenderCodeBaseProps) {
+  return (
+    <React.Suspense fallback={<Box component='code' sx={{ p: 1.5, display: 'block', ...props.sx }} />}>
+      <RenderCodeDynamic {...props} />
+    </React.Suspense>
+  );
+}
+
+export const RenderCodeMemo = React.memo(RenderCode);
