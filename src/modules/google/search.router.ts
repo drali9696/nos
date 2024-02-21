67b76eca3225ac269c38ae07af1293cabec1f2b8
alieali <121768238+alieali@users.noreply.github.com>
2024-02-21 19:04:05 +0330

    Update search.router.ts

--- src/modules/google/search.router.ts
+++ src/modules/google/search.router.ts
@@ -3,7 +3,7 @@ import { z } from 'zod';
 
 import { createTRPCRouter, publicProcedure } from '~/server/api/trpc.server';
 import { env } from '~/server/env.mjs';
-import { fetchJsonOrTRPCError } from '~/server/api/trpc.router.fetchers';
+import { fetchJsonOrTRPCError } from '~/server/api/trpc.serverutils';
 
 import { Search } from './search.types';
 
